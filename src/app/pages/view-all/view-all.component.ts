6f8cd19b585da3302b16578ad76a3279c8b4300b
carolliie <anacvieira1415@gmail.com>
2024-09-29 14:25:19 -0300

    fix: search bar &  responsive posts

--- src/app/pages/view-all/view-all.component.ts
+++ src/app/pages/view-all/view-all.component.ts
@@ -2,23 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './view-all.component.html',
   styleUrl: './view-all.component.css'
 })
-export class ViewAllComponent implements OnInit{
+export class ViewAllComponent implements OnInit {
 
   allPosts: any;
   post: any;
   images: { [key: string]: string } = {};
-  constructor(private postService: PostService){}
+  searchTerm: string = '';
+
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
     this.getAllPosts();
+    this.filterPosts();
   }
 
   getAllPosts() {
@@ -39,12 +43,23 @@ export class ViewAllComponent implements OnInit{
     })
   }*/
 
-    getPostBySlug(slug:string) {
-      this.postService.getPostBySlug(slug).subscribe(res => {
-        console.log(res);
-        this.post = res;
-      }, error => {
-        console.error('Method not implemented.');
-      })
+  getPostBySlug(slug: string) {
+    this.postService.getPostBySlug(slug).subscribe(res => {
+      console.log(res);
+      this.post = res;
+    }, error => {
+      console.error('Method not implemented.');
+    })
+  }
+
+  filterPosts() {
+    if (this.searchTerm) {
+      this.allPosts = this.allPosts.filter((post: { name: string; tags: any[]; }) =>
+        post.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        post.tags && post.tags.some((tag: string) => tag.toLowerCase().includes(this.searchTerm.toLowerCase()))
+      );
+    } else {
+      this.getAllPosts();
     }
+  }
 }
