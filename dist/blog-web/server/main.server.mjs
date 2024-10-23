14cc357ee241f74ac7cf23da53e18b207de3297e
carolliie <anacvieira1415@gmail.com>
2024-10-23 17:22:55 -0300

    feat: last commit build -> netifly

--- dist/blog-web/server/main.server.mjs
+++ dist/blog-web/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-XN7TVHEJ.mjs";import"./chunk-LR3BHVMS.mjs";import"./chunk-SYK46DLG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MFD3L53H.mjs";import"./chunk-LR3BHVMS.mjs";import"./chunk-SYK46DLG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
