56b6fde5d51e4c6dc7953dc9e70bc6565e10b9b0
carolliie <anacvieira1415@gmail.com>
2024-10-30 18:01:53 -0300

    feat: build commit

--- dist/blog-web/server/main.server.mjs
+++ dist/blog-web/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-X4H567NU.mjs";import"./chunk-LR3BHVMS.mjs";import"./chunk-SYK46DLG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-QER735MH.mjs";import"./chunk-LR3BHVMS.mjs";import"./chunk-SYK46DLG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
